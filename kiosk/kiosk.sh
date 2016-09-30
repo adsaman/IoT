aeb8fa2fd6e395d9d2eee1ac166510cdfb582011
Adrián Santos Marrero <adsaman@gmail.com>
2016-09-30 11:04:18 +0100

    Epiphany no soporta parámetros por defecto en JS

--- kiosk/kiosk.sh
+++ kiosk/kiosk.sh
@@ -3,7 +3,7 @@ xset -dpms 	# disable DPMS (Energy Star) features
 xset s off 	# disable screen saver
 xset s noblank 	# don't blank the video device
 unclutter &	# hide mouse cursor
-matchbox-window-manager &
+matchbox-window-manager -use_titlebar no &
 while true; do
 	#midori -e Fullscreen -a $HOME/IoT/kiosk/index.html
 	epiphany-browser -a --profile ~/.config ~/IoT/kiosk/index.html
