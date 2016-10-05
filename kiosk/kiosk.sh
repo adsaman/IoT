30e857009fccdc809dad400f22693884e3dcb531
Adrián Santos Marrero <adsaman@gmail.com>
2016-10-05 12:36:49 +0100

    Añadiendo opciones a la ejecución de Chromium

--- kiosk/kiosk.sh
+++ kiosk/kiosk.sh
@@ -5,5 +5,9 @@ xset s noblank 	# don't blank the video device
 unclutter &	# hide mouse cursor
 matchbox-window-manager -use_titlebar no &
 while true; do
-	chromium-browser --incognito --kiosk "file://$HOME/IoT/kiosk/index.html" 
+	chromium-browser --kiosk --incognito --no-first-run \
+		--disable-java --disable-plugins \
+		--disk-cache-size=$((1024 * 1024)) --disk-cache-dir=/tmp/chromium/ \
+		--user-data-dir=/tmp/chromium/ \
+		--noerrdialogs "file://$HOME/IoT/kiosk/index.html" 
 done
